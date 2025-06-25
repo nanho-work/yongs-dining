62c7b0869242216d0b578bcd3786d1a976c8dd9a
nhchoi2 <choi1278@gmail.com>
2025-06-25 17:30:21 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/yongs-dining" : "",
+  assetPrefix: isProd ? "/yongs-dining" : "",
 };
 
 export default nextConfig;
